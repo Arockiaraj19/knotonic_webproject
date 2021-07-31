2b78e1231df57358ae540744714cccc302ab5556
Arockia19 <arockiaraj@kriyatec.com>
2021-07-31 09:51:48 +0530

    adding scroll animation

--- test.js
+++ test.js
@@ -1,36 +1,48 @@
+function LottieScrollTrigger(vars) {
+  let playhead = {frame: 0},
+    target = gsap.utils.toArray(vars.target)[0],
+    speeds = {slow: "+=2000", medium: "+=1000", fast: "+=500"},
+    st = {trigger: target, pin: true, start: "top top", end: speeds[vars.speed] || "+=1000", scrub: 1},
+    animation = lottie.loadAnimation({
+      container: target,
+      renderer: vars.renderer || "svg",
+      loop: false,
+      autoplay: false,
+      path: vars.path
+    });
+  for (let p in vars) { // let users override the ScrollTrigger defaults
+    st[p] = vars[p];
+  }
+  animation.addEventListener("DOMLoaded", function() {
+    gsap.to(playhead, {
+      frame: animation.totalFrames - 1,
+      ease: "none",
+      onUpdate: () => animation.goToAndStop(playhead.frame, true),
+      scrollTrigger: st
+    });
+  });
+}
 
+function animation() {
+  
+  const animationContainer = document.querySelector('#threecontainer')
 
-  var animData = {
-  container: document.getElementById('threecontainer'),
-  path: 'data.json',
-  renderer: 'svg',
-  loop: false,
-  autoplay: false,
-  name: "animScroll",
-}, animScroll, tl;
-
-
-var animScroll = bodymovin.loadAnimation(animData)
-
-
-animScroll.addEventListener('DOMLoaded', function () {
-  console.log("function loaded");
-  tl = new TimelineMax({repeat: 0})
-  tl.to({frame: 0}, 1, {
-    frame: animScroll.totalFrames-1,
-    onUpdate: function() {
-      animScroll.goToAndStop(Math.round(this.target.frame), true)
-    },
-    Ease:Linear.easeNone
+  LottieScrollTrigger({
+    target: animationContainer,
+    path: 'data.json',
+    speed: 'medium',
+    scrub: 1,
+    onUpdate: (self) => {
+     
+    }
   })
   
-  var controller = new ScrollMagic.Controller();
+}
 
-var scene = new ScrollMagic.Scene({
-  triggerElement: "#home",
- 
-  duration: 1000 }).setTween(tl).addTo(controller);
 
+
+// wait until DOM is ready
+document.addEventListener("DOMContentLoaded", function(e) {
+  // make sure window is always at top on reload
+  animation();
 })
-
-
