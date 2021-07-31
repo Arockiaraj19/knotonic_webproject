60b5d1b56c95d04f84eee8dd20c6de427712c00b
Arockia19 <arockiaraj@kriyatec.com>
2021-07-31 10:25:16 +0530

    complete 3d slide

--- script.js
+++ script.js
@@ -452,11 +452,67 @@ const setVisible = (elementOrSelector, visible) =>
 setVisible('.mainwrapper', false);
 setVisible('.wrapper', true);
 
-document.addEventListener('DOMContentLoaded', () =>
-  wait(1000).then(() => {
-    setVisible('.mainwrapper', true);
-    setVisible('.wrapper', false);
-  }));
+document.addEventListener('DOMContentLoaded', () =>{
+  
+    wait(1000).then(() => {
+      setVisible('.mainwrapper', true);
+      setVisible('.wrapper', false);
+      animation();
+  }
+  
+  
+  )});
+
+
+
+
+
+
+  function LottieScrollTrigger(vars) {
+    let playhead = {frame: 0},
+      target = gsap.utils.toArray(vars.target)[0],
+      speeds = {slow: "+=2000", medium: "+=1000", fast: "+=500"},
+      st = {trigger: target, pin: true, start: "top top", end: speeds[vars.speed] || "+=1000", scrub: 1},
+      animation = lottie.loadAnimation({
+        container: target,
+        renderer: vars.renderer || "svg",
+        loop: false,
+        autoplay: false,
+        path: vars.path
+      });
+    for (let p in vars) { // let users override the ScrollTrigger defaults
+      st[p] = vars[p];
+    }
+    animation.addEventListener("DOMLoaded", function() {
+      gsap.to(playhead, {
+        frame: animation.totalFrames - 1,
+        ease: "none",
+        onUpdate: () => animation.goToAndStop(playhead.frame, true),
+        scrollTrigger: st
+      });
+    });
+  }
+let istrue=false;
+  function animation() {
+    
+    const animationContainer = document.querySelector('#threecontainer')
+  
+    LottieScrollTrigger({
+      target: animationContainer,
+      path: 'data.json',
+      speed: 'medium',
+      scrub: 1,
+      onUpdate: (self) => {
+          istrue=!istrue;
+          console.log("enna self ithu");
+       console.log(self);
+      }
+    })
+    
+  }
+  
+
+
 
 
 
