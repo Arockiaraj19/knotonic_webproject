32fd0bd46b8ece5126157817f72ca62275e3e3b4
Arockia19 <arockiaraj@kriyatec.com>
2021-08-02 20:24:02 +0530

    adding space between

--- script.js
+++ script.js
@@ -421,8 +421,8 @@ var contentinit = ["don mobile", "don lucifer", "don web", "don cloud computing"
 var images=["url(./assets/sliderp1.png)","url(./assets/desktopslider.png)","url(./assets/webappslider.png)","url(./assets/process1.png)"];
 var contentinit = ["don mobile", "A Web Application Is A Client-Server Program. It Means That It Has A Client-Side And A Server-Side. It Is Part Of The Client-Server Environment, Where Many Computers Share Information. ", "don web", "don cloud computing"];
 
-var bgcolors=["red","white","yellow","black"];
-var backcolors=["red","white","yellow","black"];
+var bgcolors=["red","orange","yellow","black"];
+var backcolors=["red","orange","yellow","black"];
 var headingcontent=[["one0","two0","three0","four0"],["one1","two1","three1","four1"],["one2","two2","three2","four2"],["one3","two3","three3","four3"],]
 const changeside = (e, i) => {
 
@@ -438,6 +438,7 @@ const changeside = (e, i) => {
         e.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.className = "flip-card";
         document.getElementById("bg_color"+i.toString()).style.backgroundColor = bgcolors[1];
         document.getElementById("back_color"+i.toString()).style.backgroundColor = backcolors[1];
+        document.getElementById("back_color"+i.toString()).style.justifyContent = "space-around";
         document.getElementById("heading"+i.toString()).innerText = headingcontent[i][1]; 
        
     } else {
@@ -454,6 +455,7 @@ const changeside = (e, i) => {
                 document.getElementById(i.toString()).innerHTML = content[i];
                 document.getElementById("image"+i.toString()).style.backgroundColor = bgcolors[2];
                 document.getElementById("backback_color"+i.toString()).style.backgroundColor = backcolors[2];
+                document.getElementById("backback_color"+i.toString()).style.justifyContent = "space-around";
                 document.getElementById("backheading"+i.toString()).innerText= headingcontent[i][2]; 
         } else {
            
@@ -464,6 +466,7 @@ const changeside = (e, i) => {
             changeposition[i] = undefined;
             document.getElementById("bg_color"+i.toString()).style.backgroundColor = bgcolors[0];
             document.getElementById("back_color"+i.toString()).style.backgroundColor = backcolors[0];
+            document.getElementById("back_color"+i.toString()).style.justifyContent = "center";
             document.getElementById("heading"+i.toString()).innerText = headingcontent[i][0]; 
         }
     }
@@ -473,6 +476,7 @@ const changebackside = (e,i) => {
     document.getElementById("bg_color"+i.toString()).style.backgroundColor = bgcolors[3];
     document.getElementById("back_color"+i.toString()).style.backgroundColor = backcolors[3];
     document.getElementById("heading"+i.toString()).innerText = headingcontent[i][3]; 
+    document.getElementById("back_color"+i.toString()).style.justifyContent = "space-around";
     e.target.offsetParent.offsetParent.style.transform = "rotateX(0deg)";
 
 }
