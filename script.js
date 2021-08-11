abe63279190cc81bdbd893677cccfe0f80822108
Arockia19 <arockiaraj@kriyatec.com>
2021-08-11 21:13:06 +0530

    adding analytics

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
+gtag('config', 'G-Z9EVMKSGF1');
 
 var slidehomeIndex = 0;
 showhomeSlides();
